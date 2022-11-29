00be0025fa15b4abd6b68f7e23aec1233bc5e339
Bruno Bessa <bebessax@gmail.com>
2022-11-29 12:00:45 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            endereco: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por  favor, insira o nome completo'
+        },
+        submitHandler: function(form) {
+            alert('Formulario enviado com sucesso!');
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+})
